3ef25d3dcbcc1e5746aee7be34b019eb0902ebe2
Michael Bestas <mkbestas@lineageos.org>
2023-03-31 07:26:47 +0300

    sm6375-common: Build missing power libs
    Change-Id: Iead8985a873e4f120fc5a84ce636eccb9051c66f

--- holi.mk
+++ holi.mk
@@ -356,7 +356,9 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service-qti \
+    android.hardware.power@1.2.vendor \
+    android.hardware.power-V1-ndk_platform.vendor
 
 # Public Libraries
 PRODUCT_COPY_FILES += \
