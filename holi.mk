082e483a000a844964e5695a31006c1bf20d0341
Michael Bestas <mkbestas@lineageos.org>
2024-06-24 00:14:42 +0300

    sm6375-common: Convert aw882xx firmware symlink to install_symlink target
    Change-Id: I1ed840d33d09e717bdc00675212fe508021dcc1c
    Signed-off-by: Vishnuvardhan Reddy <vvr-rockstar@bmsit.in>

--- holi.mk
+++ holi.mk
@@ -69,6 +69,7 @@ PRODUCT_PACKAGES += \
     audio.primary.holi \
     audio.r_submix.default \
     audio.usb.default \
+    firmware_aw_cali.bin_symlink \
     libaudiopreprocessing \
     libaudioroute \
     libbatterylistener \
