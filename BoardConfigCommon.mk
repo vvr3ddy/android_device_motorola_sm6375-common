4aec53daaeb7984f9a9cb2d67a52e158f16a59c0
Vishnuvardhan Reddy <vvr-rockstar@bmsit.in>
2024-07-23 12:24:15 +0100

    sm6375-common: Configure Kernel flags
    Signed-off-by: Vishnuvardhan Reddy <vvr-rockstar@bmsit.in>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -20,6 +20,20 @@ TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := generic
 TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a75
 
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := holi
+TARGET_NO_BOOTLOADER := true
+
+# Kernel
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0x4a90000 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=2048 loop.max_part=7
+BOARD_KERNEL_PAGESIZE := 4096
+TARGET_KERNEL_CONFIG := vendor/holi-qgki_defconfig
+TARGET_KERNEL_SOURCE := kernel/motorola/sm6375
+
+# Kernel - Prebuilt
+include device/motorola/sm6375-kernel/prebuilt_kernel.mk
+
 # Platform
 TARGET_BOARD_PLATFORM := holi
 
