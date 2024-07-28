1939dad13c234c602f09bba0737c7e3fb37a3820
Vishnuvardhan Reddy <vvr-rockstar@bmsit.in>
2024-07-28 21:07:14 +0100

    sm6375-common: Initial Device Tree Structure  - Add Arch information from Code Linaro  - Set board platform  - Set Shipping API Levels  - Add Bootloader Flags  - Configure Virtual A/B partitions  - Set up partition flags  - Enable vendor_boot partition and build  - Set up and build DTBO Signed-off-by: Vishnuvardhan Reddy <vvr-rockstar@bmsit.in>

---
+++ README.md
@@ -0,0 +1,7 @@
+# Common device tree for Motorola SM6375 devices
+
+
+```
+Copyright (C) 2024 The LineageOS Project
+SPDX-License-Identifier: Apache-2.0
+```
---
+++ holi.mk
@@ -0,0 +1,49 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# A/B
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
+
+AB_OTA_UPDATER := true
+
+AB_OTA_PARTITIONS += \
+    boot \
+    dtbo \
+    product \
+    system \
+    system_ext \
+    vendor \
+    vendor_boot
+
+PRODUCT_PACKAGES += \
+    update_engine \
+    update_engine_sideload \
+    update_verifier
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+PRODUCT_PACKAGES += \
+    otapreopt_script
+
+# Partitions
+PRODUCT_BUILD_SUPER_PARTITION := false
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Shipping API level
+BOARD_SHIPPING_API_LEVEL := 30
+PRODUCT_SHIPPING_API_LEVEL := 31
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# Vendor ramdisk
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/fstab.qcom_ramdisk:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
