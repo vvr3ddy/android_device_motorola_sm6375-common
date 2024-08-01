4483eb481548854bbc3752b4a9781242aa2bfbda
Vishnuvardhan Reddy <vvr-rockstar@bmsit.in>
2024-08-01 21:41:02 +0100

    sm6375-common: Build Keymaster Libs
    The Keymaster HAL acts as an interface between the Android operating system
    and the vendor-specific Keymaster implementation, which is typically part
    of the Trusted Execution Environment (TEE) or a similar secure hardware module.
    
    Signed-off-by: Vishnuvardhan Reddy <vvr-rockstar@bmsit.in>

--- holi.mk
+++ holi.mk
@@ -175,6 +175,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor
 
+# Keymaster
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@4.1.vendor
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
