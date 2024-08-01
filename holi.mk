6cf9cabbd04c4577db7969a7aa24900877c77a6e
Vishnuvardhan Reddy <vvr-rockstar@bmsit.in>
2024-08-01 21:48:53 +0100

    sm6375-common: Add vendor.qti.hardware.perf@2.2.vendor package
    This package provides performance management capabilities,
    allowing the system to optimize and manage performance-related
    parameters for Qualcomm-based devices.
    
    Signed-off-by: Vishnuvardhan Reddy <vvr-rockstar@bmsit.in>

--- holi.mk
+++ holi.mk
@@ -243,6 +243,10 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Perf
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.perf@2.2.vendor
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.pro.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.pro.xml \
