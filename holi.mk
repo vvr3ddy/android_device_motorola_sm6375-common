fc51d187fa3feeca899bb12a982128fb9c9cc44a
Vishnuvardhan Reddy <vvr-rockstar@bmsit.in>
2024-08-04 00:30:55 +0100

    sm6375-common: Fix typo in public libraries
    Signed-off-by: Vishnuvardhan Reddy <vvr-rockstar@bmsit.in>

--- holi.mk
+++ holi.mk
@@ -292,7 +292,7 @@ PRODUCT_PACKAGES += \
 # Public Libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt \
-    $(LOCAL_PATH)/configs/public.libraries.-qti.system_ext.txt:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/public.libraries-qti.txt
+    $(LOCAL_PATH)/configs/public.libraries-qti.system-ext.txt:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/public.libraries-qti.txt
 
 # RIL
 PRODUCT_PACKAGES += \
