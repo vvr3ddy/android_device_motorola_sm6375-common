107505e381ed2de9b5eb3b4d94551529f93187a1
Vishnuvardhan Reddy <vvr-rockstar@bmsit.in>
2024-08-04 00:23:22 +0100

    sm6375-common: Import FCM based on error log.  - Further fix up compat matrix and manifest for build errors
    Signed-off-by: Vishnuvardhan Reddy <vvr-rockstar@bmsit.in>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -72,6 +72,7 @@ TARGET_FS_CONFIG_GEN := \
 # HIDL
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(COMMON_PATH)/framework_compatibility_matrix.xml
 
 # Kernel
 BOARD_BOOT_HEADER_VERSION := 3
