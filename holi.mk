97e0d634ff60bba8ea30f02a0e4323851b058f64
Vishnuvardhan Reddy <vvr-rockstar@bmsit.in>
2024-09-09 10:55:19 +0100

    sm6375-common: use regular fstab for vendor ramdisk as well
    Signed-off-by: Vishnuvardhan Reddy <vvr-rockstar@bmsit.in>

--- holi.mk
+++ holi.mk
@@ -440,7 +440,7 @@ PRODUCT_PACKAGES += \
 
 # Vendor ramdisk
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/etc/fstab.qcom_ramdisk:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
+    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
 
 # Vendor service manager
 PRODUCT_PACKAGES += \
