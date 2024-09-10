2f5c0655b8431916545ec55af1764aa0009b648b
Vishnuvardhan Reddy <vvr-rockstar@bmsit.in>
2024-09-10 17:02:42 +0100

    sm6375-common: Add AV services and codec2 HIDL dependencies
     - Include libavservices_minijail and codec2 HIDL libraries in the build.
       These additions will resolve linking issues for various vendor binaries
       and improve media processing capabilities.
    
    Signed-off-by: Vishnuvardhan Reddy <vvr-rockstar@bmsit.in>

--- holi.mk
+++ holi.mk
@@ -235,6 +235,11 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
+PRODUCT_PACKAGES += \
+    libavservices_minijail \
+    libavservices_minijail.vendor \
+    libcodec2_hidl@1.0.vendor
+
 # Net
 PRODUCT_PACKAGES += \
     android.system.net.netd@1.1.vendor
