b4c8814360dd0f7fc5f8c2176e454dea10ca0315
Vishnuvardhan Reddy <vvr-rockstar@bmsit.in>
2024-10-01 22:38:16 +0100

    sm6375-common: Build Renderscript packages
     - RenderScript is a framework for running computationally intensive
       tasks at high performance on Android. It is designed for use with
       data-parallel computation, although serial workloads can benefit
       as well. The RenderScript runtime parallelizes work across processors
       available on a device, such as multi-core CPUs and GPUs, enabling
       developers to focus on expressing algorithms rather than scheduling
       work. RenderScript is especially useful for apps performing image
       processing, computational photography, or computer vision.
    
     More Info: https://source.android.com/docs/core/architecture/vndk/renderscript
    
    Signed-off-by: Vishnuvardhan Reddy <vvr-rockstar@bmsit.in>

--- holi.mk
+++ holi.mk
@@ -356,6 +356,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt \
     $(LOCAL_PATH)/configs/public.libraries-qti.system-ext.txt:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/public.libraries-qti.txt
 
+# RenderScript
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0-impl
+
 # RFS MSM MPSS symlinks
 PRODUCT_PACKAGES += \
     rfs_msm_mpss_readonly_vendor_fsg_symlink
